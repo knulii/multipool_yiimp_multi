228ecbe34adbf2f55050123f03370361ca9124d0
knulii <55326439+knulii@users.noreply.github.com>
2024-10-13 17:07:38 -0700

    Update system_multi_db.sh

--- system_multi_db.sh
+++ system_multi_db.sh
@@ -27,12 +27,9 @@ echo -e "$GREEN Done...$COL_RESET"
 
 # MariaDB
 echo -e " Installing MariaDB Repository...$COL_RESET"
-hide_output sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
-if [[ ("$DISTRO" == "16") ]]; then
-  sudo add-apt-repository 'deb [arch=amd64,arm64,i386,ppc64el] http://mirror.one.com/mariadb/repo/10.4/ubuntu xenial main' >/dev/null 2>&1
-else
-  sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mirror.one.com/mariadb/repo/10.4/ubuntu bionic main' >/dev/null 2>&1
-fi
+sudo apt-get install software-properties-common
+hide_output sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://sfo1.mirrors.digitalocean.com/mariadb/repo/10.4/ubuntu focal main' >/dev/null 2>&1
 echo -e "$GREEN Done...$COL_RESET"
 
 # Upgrade System Files
